cc1d87a624fba26a30dfcae85b698654e114bbc9
Tushar1401 <tushararora1401@gmail.com>
2020-08-22 09:47:26 +0530

    minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyBUV7hRJUex1dROI_TV5kl0jR50mV9PkTw",
-        authDomain: "facebook-messenger-clone-df482.firebaseapp.com",
-        databaseURL: "https://facebook-messenger-clone-df482.firebaseio.com",
-        projectId: "facebook-messenger-clone-df482",
-        storageBucket: "facebook-messenger-clone-df482.appspot.com",
-        messagingSenderId: "137385043628",
-        appId: "1:137385043628:web:09e12a15ba15881c2b6109",
-        measurementId: "G-7W3XH15XKP"
+        apiKey: "Put your own ones",
+        authDomain: "Put your own ones",
+        databaseURL: "Put your own ones",
+        projectId: "Put your own ones",
+        storageBucket: "Put your own ones",
+        messagingSenderId: "Put your own ones",
+        appId: "Put your own ones",
+        measurementId: "Put your own ones"
 })
 
 const db = firebaseApp.firestore();
